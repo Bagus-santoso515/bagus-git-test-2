6ab14cf9446df63b79b5d2218ca6686ea47280e4
Bagus Santoso <bagus.santoso515@gmail.com>
2022-02-02 21:41:20 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi ipsum earum voluptates possimus tenetur cum! Neque magni quis nesciunt est dignissimos voluptate vel atque cum natus? Nam ab officiis repudiandae?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
